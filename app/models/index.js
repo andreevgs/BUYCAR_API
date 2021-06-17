8f15e4603a3d8779b8e5c91ac3533b8372d401a8
Gleb <thetimm8890@gmail.com>
2021-06-17 16:09:30 +0300

    add posts

--- app/models/index.js
+++ app/models/index.js
@@ -24,6 +24,8 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
+db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+
 db.user = require("../models/user.model.js")(sequelize, Sequelize);
 db.offer = require("../models/offer")(sequelize, Sequelize);
 db.role = require("../models/role.model.js")(sequelize, Sequelize);
